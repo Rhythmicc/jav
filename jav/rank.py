ef95e6866336653c8615b06837835323ab124349
Rhythmicc <RhythmLian@outlook.com>
2022-10-26 13:29:29 +0800

    update

--- jav/rank.py
+++ jav/rank.py
@@ -1,24 +1,27 @@
 import time
-from . import info_baseUrl
+from . import info_baseUrl, translate
 
 companies = {
-    'S1 NO.1 STYLE': f'{info_baseUrl}/studio/763?page=',
-    'Prestige': f'{info_baseUrl}/studio/671?page=',
-    'SOD Create': f'{info_baseUrl}/studio/1334?page=',
-    'Faleno': f'{info_baseUrl}/studio/4411?page=',
-    'MOODYZ': f'{info_baseUrl}/studio/294?page=',
-    'IDEA POCKET': f'{info_baseUrl}/studio/109?page=',
+    "S1 NO.1 STYLE": f"{info_baseUrl}/studio/763?page=",
+    "Prestige": f"{info_baseUrl}/studio/671?page=",
+    "SOD Create": f"{info_baseUrl}/studio/1334?page=",
+    "Faleno": f"{info_baseUrl}/studio/4411?page=",
+    "MOODYZ": f"{info_baseUrl}/studio/294?page=",
+    "IDEA POCKET": f"{info_baseUrl}/studio/109?page=",
 }
 
 
 def ask_company():
     from QuickProject import _ask
-    return _ask({
-        'type': 'list',
-        'name': 'company',
-        'message': '请选择公司',
-        'choices': list(companies.keys())
-    })
+
+    return _ask(
+        {
+            "type": "list",
+            "name": "company",
+            "message": "请选择公司",
+            "choices": list(companies.keys()),
+        }
+    )
 
 
 def get_page(company: str, page: int):
@@ -28,38 +31,40 @@ def get_page(company: str, page: int):
     url = companies[company]
     infos = []
     retry = 3
-    
+
     from . import QproDefaultConsole, QproErrorString
     from QuickProject import QproWarnString
 
-    with QproDefaultConsole.status('正在获取榜单...'):
+    with QproDefaultConsole.status("正在获取榜单..."):
         while retry:
             try:
-                r = requests.get(url + f'{page}')
+                r = requests.get(url + f"{page}")
                 if r.status_code == 200:
                     break
             except:
-                QproDefaultConsole.print(QproWarnString, '获取失败，正在重试...')
+                QproDefaultConsole.print(QproWarnString, "获取失败，正在重试...")
             finally:
                 retry -= 1
                 time.sleep(1)
     if r.status_code != 200:
-        QproDefaultConsole.print(QproErrorString, '获取榜单失败, 请检查网络连接!')
+        QproDefaultConsole.print(QproErrorString, "获取榜单失败, 请检查网络连接!")
         return None
-    soup = BeautifulSoup(r.text, 'html.parser')
-    for info in soup.find_all('a', class_='work'):
-        designation = info.find('h4', class_='work-id').text.strip()
-        title = info.find('h4', class_='work-title').text.strip()
-        _ls = info.find_all('span')
+    soup = BeautifulSoup(r.text, "html.parser")
+    for info in soup.find_all("a", class_="work"):
+        designation = info.find("h4", class_="work-id").text.strip()
+        title = info.find("h4", class_="work-title").text.strip()
+        _ls = info.find_all("span")
         date = _ls[1].text.strip()
-        actress = '未知'
+        actress = "未知"
         if len(_ls) > 2:
             actress = _ls[2].text.strip()
 
-        infos.append({
-            'designation': designation.upper(),
-            'title': title,
-            'date': date,
-            'actress': actress
-        })
+        infos.append(
+            {
+                "designation": designation.upper(),
+                "title": translate(title),
+                "date": date,
+                "actress": actress,
+            }
+        )
     return infos
